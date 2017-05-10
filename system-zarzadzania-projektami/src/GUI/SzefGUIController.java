1ce16cc65ed393bc17fcb1d57233e70e8d7b7536
matusiewicz <przemekmatusiewicz@gmail.com>
2017-05-10 22:58:56 +0200

    usuwanie usera

--- system-zarzadzania-projektami/src/GUI/SzefGUIController.java
+++ system-zarzadzania-projektami/src/GUI/SzefGUIController.java
@@ -50,14 +50,29 @@ public class SzefGUIController implements Initializable {
     }
 
     @FXML
-    private void deleteUser(ActionEvent event) throws IOException {
-        Parent loader = FXMLLoader.load(getClass().getResource("DeleteUser.fxml"));
+    private void deleteUser(ActionEvent event) throws IOException,SQLException {
+        /*Parent loader = FXMLLoader.load(getClass().getResource("DeleteUser.fxml"));
         Scene info_scene= new Scene(loader);
         Stage info_stage =new Stage();
         info_stage.setScene(info_scene);
         info_stage.initModality(Modality.APPLICATION_MODAL);
         info_stage.initOwner(deleteUser.getScene().getWindow());
-        info_stage.showAndWait();
+        info_stage.showAndWait();*/
+
+        DataPracownicy person = pracownicyTable.getSelectionModel().getSelectedItem();//obiekt DataPracownicy zaznaczonego wiersza
+        String id = person.getPracownicyTable_id().toString();
+        System.out.println("id"+id);
+
+        try {
+            String query = " DELETE FROM szp.pracownicy WHERE ID_Pracownik='"+id+"'";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.executeUpdate();
+            System.out.println("Rekord został usunięty z tabeli pracownicy!");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        parsePracownicy();
     }
 
     @FXML
