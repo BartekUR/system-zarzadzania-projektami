266a065f8ed7a475c6afac52427c0658692258f0
Michal_Korczynski <kurak1993@gmail.com>
2017-05-11 06:32:43 +0200

    Update EditUserController.java
    wyskakuje syntax w sqlu i nie ma zrobionych na razie warunków

--- system-zarzadzania-projektami/src/GUI/EditUserController.java
+++ system-zarzadzania-projektami/src/GUI/EditUserController.java
@@ -1,14 +1,35 @@
 package GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * Created by Michal on 2017-03-22.
  */
-public class EditUserController {
-    @FXML
-    private javafx.scene.control.Button closeButton;
+public class EditUserController implements Initializable {
+    public TextField euImie;
+    public TextField euNazwisko;
+    public ChoiceBox euStanowisko;
+    public TextField euNoweHaslo;
+    private SqlConnect sc = new SqlConnect();
+    private Connection conn = sc.getConn();
+    @FXML private javafx.scene.control.Button closeButton;
+    @FXML private ComboBox comboBoxWyborUzytkownika;
     @FXML
     private void closeButtonAction(){
 
@@ -16,4 +37,65 @@ public class EditUserController {
 
         stage.close();
     }
+    @FXML
+    private void fillcomboBoxDU() throws SQLException, IOException {
+        final ObservableList<Integer> options = FXCollections.observableArrayList();
+        String query = "SELECT `ID_Pracownik` FROM `szp`.`pracownicy`;";
+        PreparedStatement preparedStmt = conn.prepareStatement(query);
+        ResultSet rs = preparedStmt.executeQuery();
+        while (rs.next()){
+
+            options.add(rs.getInt("ID_Pracownik"));
+        }
+        comboBoxWyborUzytkownika.setItems(options);
+        //conn.close();
+        //rs.close();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        euStanowisko.setItems(euStanowiskoList);
+        euStanowisko.setValue("mm");
+        try {
+            fillcomboBoxDU();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editButtonAction(ActionEvent actionEvent) throws SQLException {
+        Object id = comboBoxWyborUzytkownika.getValue().toString();
+        String imie = euImie.getText();
+        String nazwisko = euNazwisko.getText();
+        Object stanowisko = euStanowisko.getValue();
+        String haslo = euNoweHaslo.getText();
+
+        try {
+
+            String query = " insert into `szp`.`pracownicy` (`Login`, `Haslo`, `Imie`, `Nazwisko`, `Stanowisko`) where `ID_Pracownik`='"+id+"'; "
+                    + " values (?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, nazwisko + imie);
+            preparedStmt.setString(2, haslo);
+            preparedStmt.setString(3, imie);
+            preparedStmt.setString(4, nazwisko);
+            preparedStmt.setObject(5, stanowisko);
+
+
+            preparedStmt.executeUpdate();
+
+            System.out.println("Rekord "+id+" został edytowany!");
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+    ObservableList <String> euStanowiskoList = FXCollections.observableArrayList("Head","Pracownik","Szef");
 }
